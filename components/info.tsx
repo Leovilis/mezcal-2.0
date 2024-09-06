fa7f009e540af2b86de8f3b4f49d7383ced0ce62
Leovilis <107759781+Leovilis@users.noreply.github.com>
2024-09-06 12:49:07 -0300

    Update info.tsx

--- components/info.tsx
+++ components/info.tsx
@@ -7,7 +7,7 @@ const Info = () => {
       <h1 className="text-bold text-3xl md:text-6xl text-center mt-5 uppercase ">
         Mariachi Puro Mezcal
       </h1>
-      <img src={mezcal.src} className="w-[300px] md:w-[400px] h-auto mt-10" />
+      <img src={mezcal.src} className="w-[300px] md:w-[400px] h-auto mt-10" alt="Puro Mezcal" />
       <h2 className="w-11/12 mt-10 p-6 uppercase md:text-justify text-xl md:text-3xl">
         MUSICALIZAMOS TODO TIPO DE EVENTOS Bautismos, Casamientos, Cumpleaños,
         quince años, Celebraciones Religiosas Argentinas, Fiestas Patronales. Lo
@@ -17,12 +17,12 @@ const Info = () => {
         musicalizar todo tipo de eventos, con lo mejor de la música tradicional
         Mexicana.
       </h2>
-      <img src={familia.src} alt="familia" className="w-[300px] md:w-[400px] h-auto mt-10" />
+      <img src={familia.src} alt="familia" className="w-[300px] md:w-[400px] h-auto mt-10" alt="En Familia" />
       <h2 className="w-11/12 mt-10 p-6 uppercase md:text-justify text-lg md:text-3xl">
         Somos músicos profesionales y nos dedicamos con alegría a musicalizar
         todo tipo de eventos, con lo mejor de la música tradicional Mexicana.
       </h2>
-      <img src={banda.src} alt="banda" className="w-[300px] md:w-[600px] h-auto mt-10" />
+      <img src={banda.src} alt="banda" className="w-[300px] md:w-[600px] h-auto mt-10" alt="Nosotros en Banda"/>
     </div>
   );
 };
