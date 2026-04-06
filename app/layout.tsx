e16c4d95dfdf4b0de2e72ad0edf71b163749f33d
Leovilis <107759781+Leovilis@users.noreply.github.com>
2026-04-06 10:47:12 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
+import { Urbanist, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const urbanist = Urbanist({ subsets: ["latin"] });
+const urbanist = Urbanist({ 
+  subsets: ["latin"],
+  variable: "--font-urbanist",
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  weight: ["400", "700", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Mariachi Puro Mezcal",
@@ -19,17 +28,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={urbanist.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        <Navbar />
-        {children}
-        <Footer />
-        </ ThemeProvider>
+      <body className={`${urbanist.variable} ${playfair.variable} font-urbanist`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
